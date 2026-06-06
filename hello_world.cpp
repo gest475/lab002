ed7b02c8738b233ebfda297a0114f5895646265f
gest475 <maryuye@mail.ru>
2026-06-06 19:33:01 +0300

    Add comments to code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
-#include <string>
+#include <iostream>  // Для ввода/вывода
+#include <string>    // Для работы со строками
 
 int main() {
-    std::string name;
+    std::string name;  // Переменная для хранения имени пользователя
+    
+    // Запрос имени у пользователя
     std::cout << "Enter your name: ";
     std::cin >> name;
+    
+    // Вывод приветствия
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
